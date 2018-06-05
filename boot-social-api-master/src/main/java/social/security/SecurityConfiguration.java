654a5ee1eee623bf6673db12623477ef71a22454
meinou <mskozlovskaya@gmail.com>
2018-06-04 22:17:02 -0400

    routing fix + frontend in static

--- boot-social-api-master/src/main/java/social/security/SecurityConfiguration.java
+++ boot-social-api-master/src/main/java/social/security/SecurityConfiguration.java
@@ -17,10 +17,15 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
+
+            //    .defaultSuccessUrl("redirect:http://localhost:3000")//
+            //    .failureUrl("/login?error=true")//
                 .antMatchers("/api/session").permitAll()
                 .antMatchers("/h2-console/**").permitAll()
                 .antMatchers("/tickets").permitAll()
                 .antMatchers("/api/**").authenticated()
+                .antMatchers("/requests").authenticated()
+
                 .and()
                 .headers().frameOptions().disable() // for h2
                 .and()
@@ -29,5 +34,9 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
                 .and().csrf().disable();
+
+        //http.authorizeRequests().and().formLogin()
+          //      .defaultSuccessUrl("http://localhost:3000");//"redirect:http://localhost:3000")
+             //   .failureUrl("http://localhost:3000/login?error=true");
     }
 }
